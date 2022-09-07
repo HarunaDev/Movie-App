f87d3e8a741be22503acef3a837bc3d3ac8367ae
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-07 13:51:34 +0100

    commented previous code
    commented previous code using traversing the dom method and created a new question object and passed in a property

--- src/questions.js
+++ src/questions.js
@@ -1,13 +1,18 @@
-const questionObject = {
-  btns: document.querySelectorAll(".question-btn"),
-  btnsClick() {
-    this.btns.forEach(function(btn) {
-      btn.addEventListener("click", function(e) {
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle("show-text");
-      })
-    })
-  }
-}
+// traversing the dom
+// const questionObject = {
+//   btns: document.querySelectorAll(".question-btn"),
+//   btnsClick() {
+//     this.btns.forEach(function(btn) {
+//       btn.addEventListener("click", function(e) {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//       })
+//     })
+//   }
+// }
 
-questionObject.btnsClick()
+// questionObject.btnsClick()
+
+const questionObject = {
+  question: document.querySelectorAll(".question")
+};
