9428b24afce2b01601c0ca6d0c7382e91f42fa1d
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-07 13:06:54 +0100

    added function for button click method
    added a function that displays current target when the method is triggered

--- src/questions.js
+++ src/questions.js
@@ -1,8 +1,12 @@
 const question = {
   btns: document.querySelectorAll(".question-btn"),
   btnsClick() {
-    
+    this.btns.forEach(function(btn) {
+      btn.addEventListener("click", function(e) {
+        console.log(e.currentTarget.parentElement);
+      })
+    })
   }
 }
 
-
+question.btnsClick()
