6584b2b8da332da42978d1817924e77c351325bd
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-07 13:20:51 +0100

    toggled the show-text class
    using the object oriented programming paradigm, I wrote a function to toggle the show-btn class in the parent element in order to display hidden text

--- src/questions.js
+++ src/questions.js
@@ -1,12 +1,13 @@
-const question = {
+const questionObject = {
   btns: document.querySelectorAll(".question-btn"),
   btnsClick() {
     this.btns.forEach(function(btn) {
       btn.addEventListener("click", function(e) {
-        console.log(e.currentTarget.parentElement);
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
       })
     })
   }
 }
 
-question.btnsClick()
+questionObject.btnsClick()
