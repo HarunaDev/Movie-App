fd001f35918737add0e820a4d249425e5942df2a
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-08 14:19:17 +0100

    added remove class function
    added function to remove show-btn class on previous question clicked when a use tries to click on another questions

--- src/questions.js
+++ src/questions.js
@@ -13,16 +13,22 @@
 
 // questionObject.btnsClick()
 
-const questionObject = {
-  questions: document.querySelectorAll(".question"), 
-  showBtn() {
-    this.questions.forEach(function(question) {
-      const btn = question.querySelector(".question-btn");
-      btn.addEventListener("click", function(){
-        question.classList.toggle("show-text")
-      })
-    })
-  }
-};
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
 
-questionObject.showBtn();
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
