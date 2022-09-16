817ef259e15b2e0f6db07decfdac3c69004e4fc3
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-16 09:44:04 +0100

    removed line gaps and added semi-colon
    for better readability

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const App = {
   mainBody: document.getElementById("movie-container"),
+  // methods
   showBanner() {
-    this.mainBody.innerHTML = ""
+    this.mainBody.innerHTML = "";
     const section = document.createElement('section')
     section.setAttribute('id', 'Banner')
     section.innerHTML = `
@@ -10,13 +11,10 @@ const App = {
     <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, voluptates? Et possimus dolor numquam in.</h3>
   
     <button>Get started</button>
-    `
-    this.mainBody.appendChild(section)
+    `;
+    this.mainBody.appendChild(section);
   },
   showReview(image,side) {
- 
-  
-
   if(side === 'right'){
     this.innerHTML = ""
     const section = document.createElement('section')
@@ -24,37 +22,36 @@ const App = {
     section.innerHTML = `
     <img src="img/${image}" alt="">
 
-        <div class="${side}">
-          <h1>Lorem ipsum dolor sit amet.</h1>
+    <div class="${side}">
+      <h1>Lorem ipsum dolor sit amet.</h1>
   
-          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, facere! Quaerat, et nostrum?</h3>
-        </div>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, facere! Quaerat, et nostrum?</h3>
+    </div>
     `;
-    this.mainBody.appendChild(section)
-
+    this.mainBody.appendChild(section);
   } else if(side === 'left'){
-      this.innerHTML = ""
+      this.innerHTML = "";
       const section = document.createElement('section')
       section.setAttribute('id',  'review')
       section.innerHTML = `
       <div class="${side}">
-              <h1>Lorem ipsum dolor sit amet.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
         
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, facere! Quaerat, et nostrum?</h3>
-            </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, facere! Quaerat, et nostrum?</h3>
+      </div>
         
-            <img src="img/${image}" alt="">
+      <img src="img/${image}" alt="">
       `;
-      this.mainBody.appendChild(section)
+      this.mainBody.appendChild(section);
   }
    return; 
   },
   renderPage() {
-    this.showBanner()
-    this.showReview("blank.png", "right")
-    this.showReview("jiraiya.png", "left")
+    this.showBanner();
+    this.showReview("blank.png", "right");
+    this.showReview("jiraiya.png", "left");
   }
-}
+};
 
-App.renderPage()
+App.renderPage();
 
