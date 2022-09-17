79434993fbcac8912e9a16d4c812b780d2a00f50
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-17 14:35:23 +0100

    added comments & cleaned up code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const App = {
   mainBody: document.getElementById("movie-container"),
   // methods
+  // show  banner method
   showBanner() {
     this.mainBody.innerHTML = "";
     const section = document.createElement('section')
@@ -14,9 +15,10 @@ const App = {
     `;
     this.mainBody.appendChild(section);
   },
+  // show review method
   showReview(image,side) {
+    // check if side is right then render the value in tempelate literals
   if(side === 'right'){
-    // this.innerHTML = ""
     const section = document.createElement('section')
     section.setAttribute('id',  'review')
     section.innerHTML = `
@@ -30,7 +32,7 @@ const App = {
     `;
     this.mainBody.appendChild(section);
   } else if(side === 'left'){
-      // this.innerHTML = "";
+    // check if side is right then render the value in tempelate literals
       const section = document.createElement('section')
       section.setAttribute('id',  'review')
       section.innerHTML = `
@@ -46,8 +48,8 @@ const App = {
   }
    return; 
   },
+  // show questions method
   showQuestions() {
-    // this.mainBody.innerHTML = ""
     const questionArr = [
       {
         question_title: "Can I stream movies on my watch-list",
@@ -61,37 +63,44 @@ const App = {
         question_title: "Are there any ads?",
         question_text: "No, the app is ad free, so you can enjoy a wonderful experience without any interruption."
       }
-    ]
+    ];
 
+    // create & append FAQ section
     const section = document.createElement('section')
     section.setAttribute('id', 'FAQ')
-    section.innerHTML = ''
-    this.mainBody.appendChild(section)
+    section.innerHTML = '';
+    this.mainBody.appendChild(section);
 
     // the code below will contain a lot of repeat, but for now the purpose is to get it to work properly, then refactor the code later for readability purpose
 
+    // create container div with header and append it to section
     const questions = document.createElement('div')
     questions.setAttribute('class', 'questions')
     questions.innerHTML = `
     <h1 class="faq-header">Frequently Asked Questions</h1>
     `;
-    section.appendChild(questions)
+    section.appendChild(questions);
 
+    // create question section wrapper and append it to container div
     const question_section = document.createElement('div')
     question_section.setAttribute('class', 'question-section')
-    question_section.innerHTML = ''
-    questions.appendChild(question_section)
+    question_section.innerHTML = '';
+    questions.appendChild(question_section);
 
+    // loop through question array and render article element according to number of objects in array
     questionArr.forEach(faQuestion => {
+      // create article element and append it to q-section wrapper
       const article = document.createElement('article')
-      article.setAttribute('class', 'question')
-      question_section.appendChild(article)
+      article.setAttribute('class', 'question');
+      question_section.appendChild(article);
 
+      // create title div with p element holding value of title from array & append to article element
       const questionTitle = document.createElement('div')
       questionTitle.setAttribute('class', 'question-title')
-      questionTitle.innerHTML = `<p>${faQuestion.question_title}</p>`
-      article.appendChild(questionTitle)
+      questionTitle.innerHTML = `<p>${faQuestion.question_title}</p>`;
+      article.appendChild(questionTitle);
 
+      // create button element holding span elements that hold p tags each & append to title div
       const button = document.createElement('button')
       button.setAttribute('class', 'question-btn')
       button.setAttribute('type', 'button')
@@ -103,23 +112,21 @@ const App = {
           <p>-</p>
         </span>
       `;
-      questionTitle.appendChild(button)
+      questionTitle.appendChild(button);
 
+      // create questionText div holding p element with the value of question-text from question array & append to article
       const questionText = document.createElement('div')
       questionText.setAttribute('class', 'question-text')
       questionText.innerHTML = `<p>${faQuestion.question_text}</p>`;
-      article.appendChild(questionText)
-
-    })
-
-    
-
+      article.appendChild(questionText);
+    });
   },
+  // render page method
   renderPage() {
     this.showBanner();
     this.showReview("blank.png", "right");
     this.showReview("jiraiya.png", "left");
-    this.showQuestions()
+    this.showQuestions();
   }
 };
 
