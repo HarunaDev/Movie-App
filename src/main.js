2562024971359a8e5705acd733d75490b83e9f39
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-17 09:36:31 +0100

    created a section for FAQ

--- src/main.js
+++ src/main.js
@@ -50,18 +50,24 @@ const App = {
     // this.mainBody.innerHTML = ""
     const questionArr = [
       {
-        "question-title": "Can I stream movies on my watch-list",
-        "question-text": "No, you can't but it is a feature that will be implemented in later versions"
+        question_title: "Can I stream movies on my watch-list",
+        question_text: "No, you can't but it is a feature that will be implemented in later versions"
       },
       {
-        "question-title": "Can I share my watch-list",
-        "question-text": "Yes, you can share watch list on different platforms for others to see."
+        question_title: "Can I share my watch-list",
+        question_text: "Yes, you can share watch list on different platforms for others to see."
       },
       {
-        "question-title": "Are there any ads?",
-        "question-text": "No, the app is ad free, so you can enjoy a wonderful experience without any interruption."
+        question_title: "Are there any ads?",
+        question_text: "No, the app is ad free, so you can enjoy a wonderful experience without any interruption."
       }
     ]
+
+    const section = document.createElement('section')
+    section.setAttribute('id', 'FAQ')
+    section.innerHTML = ''
+    this.mainBody.appendChild(section)
+
   },
   renderPage() {
     this.showBanner();
