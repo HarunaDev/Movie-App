b263dde2b762400ea727d37ce12550acdbc353a4
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-18 02:09:06 +0100

    created search input & appended to form

--- src/main.js
+++ src/main.js
@@ -11,10 +11,18 @@ const App = {
     header_left.innerHTML = `<h1>O</h1>`;
     this.header.appendChild(header_left);
 
+    // created form & appended to header_left div
     const form = document.createElement('form')
-    form.setAttribute('id', 'form')
-    header_left.appendChild(form)
+    form.setAttribute('id', 'form');
+    header_left.appendChild(form);
 
+    // created input with attributes and appended to form
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'Search')
+    input.setAttribute('id', 'search')
+    input.setAttribute('class', 'search');
+    form.appendChild(input);
   },
   // show  banner method
   showBanner() {
