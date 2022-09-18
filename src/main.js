b10f501fa039c7ce6074f0d1346a4221913138be
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-18 02:27:21 +0100

    created showFooter method
    created showFooter method that renders footer content to footer element to ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const App = {
   mainBody: document.getElementById("movie-container"),
   header: document.getElementById("header-container"),
+  footer: document.getElementById("footer"),
   // methods
   // show header method
   showHeader() {
@@ -144,6 +145,44 @@ const App = {
       article.appendChild(questionText);
     });
   },
+  // show footer method
+  showFooter() {
+    // created section with footer elements and appended it to footer
+    const section = document.createElement('section')
+    section.setAttribute('class', 'footer')
+    section.innerHTML = `
+    <h1 class="footer-header">Questions? Contact Us</h1>
+    <div class="footer-links">
+      <div class="first-col">
+        <ul>
+          <li>Investor Relations</li>
+          <li>FAQ</li>
+          <li>Privacy</li>
+          <li>Speed Test</li>
+        </ul>
+      </div>
+
+      <div class="first-col">
+        <ul>
+          <li>Help Center</li>
+          <li>Cookies</li>
+          <li>Jobs</li>
+          <li>Legal Notice</li>
+        </ul>
+      </div>
+
+      <div class="first-col">
+        <ul>
+          <li>Accounts</li>
+          <li>Media Center</li>
+          <li>Terms of Use</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+    `;
+    this.footer.appendChild(section);
+  },
   // render page method
   renderPage() {
     this.showHeader();
@@ -151,6 +190,7 @@ const App = {
     this.showReview("blank.png", "right");
     this.showReview("jiraiya.png", "left");
     this.showQuestions();
+    this.showFooter();
   }
 };
 
