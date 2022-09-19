fd29031237219b9e4b16830bfa63b0bed56f6868
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-19 18:48:21 +0100

    imported showHeader module
    to break app into modules

--- src/main.js
+++ src/main.js
@@ -1,29 +1,11 @@
+import { showHeader } from "./showHeader.js";
+
 const App = {
   mainBody: document.getElementById("movie-container"),
   header: document.getElementById("header-container"),
   footer: document.getElementById("footer"),
   // methods
-  showHeader() {
-    // created show header method to display render header elements
-    this.header.innerHTML = `<h1>Show<span>Plus</span> </h1>`;
-    const header_left = document.createElement('div')
-    header_left.setAttribute('class', 'left')
-    header_left.innerHTML = `<h1>O</h1>`;
-    this.header.appendChild(header_left);
-  
-    // created form & appended to header_left div
-    const form = document.createElement('form')
-    form.setAttribute('id', 'form');
-    header_left.appendChild(form);
-  
-    // created input with attributes and appended to form
-    const input = document.createElement('input')
-    input.setAttribute('type', 'text')
-    input.setAttribute('placeholder', 'Search')
-    input.setAttribute('id', 'search')
-    input.setAttribute('class', 'search');
-    form.appendChild(input);
-  },
+  showHeader,
   // show  banner method
   showBanner() {
     this.mainBody.innerHTML = "";
@@ -222,3 +204,4 @@ const App = {
 
 App.renderPage();
 
+export default App;
