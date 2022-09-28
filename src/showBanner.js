de4d68a73eb1db3d7c73fb48090da45714594ac2
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-28 16:22:09 +0100

    refactored showBanner method and styling

--- src/showBanner.js
+++ src/showBanner.js
@@ -5,9 +5,9 @@ export function showBanner() {
   const section = document.createElement('section')
   section.setAttribute('id', 'Banner')
   section.innerHTML = `
-  <h1>Lorem ipsum dolor sit amet.</h1>
+  <h1>Unlimited movies, TV <br> shows and more.</h1>
 
-  <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, voluptates? Et possimus dolor numquam in.</h3>
+  <h3>Create a watch-list, share with friends.</h3>
 
   <button>Get started</button>
   `;
