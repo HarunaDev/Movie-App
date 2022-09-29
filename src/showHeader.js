ee1097049192c7695527bccd444c140d3b75e9ec
Alvin Haruna <harunaalvin69@gmail.com>
2022-09-29 15:15:13 +0100

    removed unused code

--- src/showHeader.js
+++ src/showHeader.js
@@ -2,7 +2,7 @@ import App from "./main.js";
 
 export function showHeader() {
   // created show header method to display render header elements
-  this.header.innerHTML = `<h1>Show<span>Plus</span> </h1>`;
+  this.header.innerHTML = `<h1>Show<span>Plus<i class="fa-regular fa-plus"></i></span>  </h1>`;
   const header_left = document.createElement('div')
   header_left.setAttribute('class', 'left')
   header_left.innerHTML = `<h1>O</h1>`;
@@ -12,6 +12,9 @@ export function showHeader() {
   const form = document.createElement('form')
   form.setAttribute('id', 'form');
   header_left.appendChild(form);
+
+  // const search_icon = document.createElement('i')
+  // search_icon.classList.add('fa-regular', '')
 
   // created input with attributes and appended to form
   const input = document.createElement('input')
