9cee461c65dd76843ee3c9bb2345aa53064ee932
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-02 12:13:43 +0100

    commented out modules, objects and methods
    commented out show movie & get movie modules, and some variable declarations for app, with certain methods

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { showHeader } from "./showHeader.js";
 import { showBanner } from "./showBanner.js";
 import { showReview } from "./showReview.js";
-import {  getMovies } from "./getMovies.js";
-import { showMovies } from "./showMovies.js";
+// import {  getMovies } from "./getMovies.js";
+// import { showMovies } from "./showMovies.js";
 import { showQuestions } from "./showQuestions.js";
 import { showFooter } from "./showFooter.js";
-import { API_KEY } from "./variable.js";
+// import { API_KEY } from "./variable.js";
 
 const App = {
   mainBody: document.getElementById("movie-container"),
@@ -13,24 +13,24 @@ const App = {
   faq: document.getElementById("faq-container"),
   footer: document.getElementById("footer"),
   base_url: "https://api.themoviedb.org/3",
-  api_url:  "/discover/movie?sort_by=popularity.desc&" + API_KEY,
+  // api_url:  "/discover/movie?sort_by=popularity.desc&" + API_KEY,
   img_url: "https://image.tmdb.org/t/p/w500",
 
   // methods
   showHeader,
   showBanner,
   showReview,
-  getMovies,
-  showMovies,
+  // getMovies,
+  // showMovies,
   showQuestions,
   showFooter,
   // render page method
  renderPage() {
   this.showHeader();
   this.showBanner();
-  this.showReview("blank.png", "right");
-  this.showReview("jiraiya.png", "left");
-  this.getMovies(this.base_url + this.api_url)
+  this.showReview("bleach-1.png", "right");
+  this.showReview("bleach-2.png", "left");
+  // this.getMovies(this.base_url + this.api_url)
   this.showQuestions();
   this.showFooter();
 }
